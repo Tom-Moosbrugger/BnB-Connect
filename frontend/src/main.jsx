5ff7dedd79785fb4c6eb8a060ae07ef14fb7a987
Tom-Moosbrugger <tamoosbrugger@gmail.com>
2025-01-09 14:25:22 -0800

    initial setup complete

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+
+const store = configureStore();
+
+if (import.meta.env.MODE !== 'production') {
+  window.store = store;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
+
