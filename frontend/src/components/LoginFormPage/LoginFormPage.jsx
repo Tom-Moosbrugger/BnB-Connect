6a141318c4c82307a3a6975e342353b2e6c15471
Tom-Moosbrugger <tamoosbrugger@gmail.com>
2025-01-09 18:34:49 -0800

    added login form

---
+++ frontend/src/components/LoginFormPage/LoginFormPage.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { loginThunk } from "../../store/session";
+import { Navigate } from 'react-router-dom'
+import './LoginForm.css';
+
+const LoginFormPage = () => {
+    const [credential, setCredential] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState({});
+    const dispatch = useDispatch();
+    const sessionUser = useSelector((state) => state.session.user);
+
+    if (sessionUser) return <Navigate to='/' replace={true}/>
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        setErrors({});
+
+        return dispatch(loginThunk({ credential, password })).catch(
+            async (res) => {
+              const data = await res.json();
+              if (data?.errors) setErrors(data.errors);
+            }
+        );
+    };
+    
+
+    return (
+        <div  className="login-wrapper">
+            <h2>Log in or sign up</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-div">
+                    <label>Username or email:</label>
+                    <input 
+                    type='text'
+                    required={true}
+                    placeholder="Enter your username or email..."
+                    value={credential}
+                    onChange={e => setCredential(e.target.value)}
+                    />
+                </div>
+                <div className="form-div">
+                    <label>Password:</label>
+                    <input 
+                    type='text'
+                    required={true}
+                    placeholder="Enter your password..."
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    />
+                </div>
+                <div className="error-div">
+                    {errors.credential && <p className="error-message">*{errors.credential}</p>}
+                </div>
+                <div className="form-div">
+                    <button>Login</button>
+                </div>
+               
+            </form>
+        </div>
+    );
+};
+
+export default LoginFormPage;
---
+++ frontend/src/store/session.js
@@ -0,0 +1,53 @@
+import { csrfFetch } from "./csrf";
+
+// action constants
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+// regular actions
+
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        user
+    }
+};
+
+const removeUser = () => {
+    return {
+        type: REMOVE_USER
+    }
+};
+
+// thunk actions
+
+export const loginThunk = (credentials) => async dispatch => {
+    const response = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify(credentials)
+    });
+
+    const data = await response.json();
+
+    dispatch(setUser(data.user));
+
+    return response;
+}
+
+// reducer
+
+const sessionReducer = (state = { user: null }, action) => {
+    switch(action.type) {
+        case SET_USER:
+            return { ...state, user: action.user };
+        case REMOVE_USER: 
+            return { ...state, user: null };
+        default:
+            return state;
+    }
+}
+
+export default sessionReducer;
+
+// store.dispatch(window.sessionActions.setUserThunk({ credential: 'demo@user.io', password: 'password' }));
