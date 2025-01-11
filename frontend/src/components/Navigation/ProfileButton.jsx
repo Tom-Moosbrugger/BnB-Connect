0fe0f4b48df1c34e438c9a8e855918a06d9ee650
Tom-Moosbrugger <tamoosbrugger@gmail.com>
2025-01-10 18:09:51 -0800

    redesigned navigation

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -2,11 +2,19 @@ import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { BsPersonCircle } from "react-icons/bs";
 import * as sessionActions from '../../store/session';
+import OpenModalButton from '../OpenModalButton';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
 
 const ProfileButton = ({ user }) => {
+    const dispatch = useDispatch();
     const [showMenu, setShowMenu] = useState(false);
-    const dispatch = useDispatch()
     const ulRef = useRef();
+
+    const toggleMenu = (e) => {
+        e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
+        setShowMenu(!showMenu);
+    };
 
     useEffect(() => {
         if (!showMenu) return;
@@ -27,28 +35,46 @@ const ProfileButton = ({ user }) => {
         dispatch(sessionActions.logoutThunk());
     };
 
-    const toggleMenu = (e) => {
-        e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
-        setShowMenu(!showMenu);
-      };
-
+    const closeMenu = () => setShowMenu(false);
+    
     const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
     return (
-        <div className="profile-icon">
-            <button onClick={toggleMenu}>
-                <BsPersonCircle />  
-            </button>
-            <ul className={ulClassName} ref={ulRef}>
+        <>
+          <button onClick={toggleMenu}>
+            <BsPersonCircle />
+          </button>
+          <ul className={ulClassName} ref={ulRef}>
+            {user ? (
+              <>
                 <li>{user.username}</li>
                 <li>{user.firstName} {user.lastName}</li>
                 <li>{user.email}</li>
                 <li>
-                    <button onClick={logout}>Log Out</button>
+                  <button onClick={logout}>Log Out</button>
                 </li>
-            </ul>
-        </div>
-    )
+              </>
+            ) : (
+              <>
+                <li>
+                  <OpenModalButton
+                    buttonText="Log In"
+                    modalComponent={<LoginFormModal />}
+                    onButtonClick={closeMenu}
+                  />
+                </li>
+                <li>
+                  <OpenModalButton
+                    buttonText="Sign Up"
+                    modalComponent={<SignupFormModal />}
+                    onButtonClick={closeMenu}
+                  />
+                </li>
+              </>
+            )}
+          </ul>
+        </>
+      );
 }
 
 export default ProfileButton;
